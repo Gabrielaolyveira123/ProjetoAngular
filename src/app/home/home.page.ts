5d991a44da4419ddcf26ef64920f9bb1573ae04a
Gabrielaolyveira123 <gabriela.oliveira.etec@gmail.com>
2024-09-09 10:50:41 +0000

    Alteração calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +8,51 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public gasolina: number | undefined;
+  public alcool: number | undefined;
+  public razao: number | undefined;
+  public resultado: string = "";
+
+  constructor(private alertCtrl: AlertController) {}
+
+  async comparar() {
+    if (this.gasolina == null || this.alcool == null) {
+      const alert = await this.alertCtrl.create({
+        header: 'Atenção',
+        message: 'Por favor, preencha os dois valores',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    if (this.gasolina <= 0 || this.alcool <= 0) {
+      const alert = await this.alertCtrl.create({
+        header: 'Inválido',
+        message: 'Os valores devem ser maiores que zero',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    if (this.gasolina === this.alcool) {
+      this.resultado = 'Ambos são vantajosos';
+      return;
+    }
+
+   this.razao = this.alcool / this.gasolina;
+    console.log('A razão é:', this.razao);
+
+    if (this.razao <= 0.7) {
+      this.resultado = 'Álcool é mais vantajoso';
+    } else {
+      this.resultado = 'Gasolina é mais vantajosa';
+    }
+    console.log('Resultado:', this.resultado);
+  }
 }
+  
