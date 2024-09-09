fed772fd78098972335486c87bc13fe539082ab1
Gabrielaolyveira123 <gabriela.oliveira.etec@gmail.com>
2024-09-09 11:54:15 +0000

    Aplicativo - Controle Gênero - Musical

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class HomePage {
+
+  public nome = "";
+
+  public items: any[] =  [
+
+{
+  titulo: "Forró"
+},
+
+{
+  titulo: "Rock"
+},
+
+{
+  titulo: "Sertanejo"
+},
+
+{
+  titulo: "Funk"
+},
+
+  ]
+
   constructor() {}
+  adicionar(){
+    //adiciona no array
+    
+      this.items.push({titulo: this.nome});
+  
+    //limpando campo nome
+    this.nome = "";
+  }
 }
