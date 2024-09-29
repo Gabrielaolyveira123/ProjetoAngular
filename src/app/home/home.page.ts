e71b278fdf74a0c8509b2a2cfc232c2617860b2f
Gabrielaolyveira123 <gabriela.oliveira.etec@gmail.com>
2024-09-29 19:21:47 +0000

    media das notas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,14 +11,14 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HomePage {
   constructor() { }
-public notaA: number | undefined;
-public notaB: number | undefined;
-public media: number | undefined;
-public situacao: string = "";
-calcularMedia() {
-if (this.notaA && this.notaB) {
-this.media = (this.notaA + this.notaB) / 2;
-this.situacao = this.media >= 7 ? 'Aprovado' : 'Reprovado';
-}
-}
+  public notaA: number | undefined;
+  public notaB: number | undefined;
+  public media: number | undefined;
+  public situacao: string = "";
+  calcularMedia() {
+    if (this.notaA && this.notaB) {
+      this.media = (this.notaA + this.notaB) / 2;
+      this.situacao = this.media >= 7 ? 'Aprovado' : 'Reprovado';
+    }
+  }
 }
