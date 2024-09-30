c1422c3be79b31feff5c66b170d4b2f2fc648eb3
Gabrielaolyveira123 <gabriela.oliveira.etec@gmail.com>
2024-09-30 11:18:23 +0000

    Gabriela e Edisom

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +8,36 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public peso: number | undefined;
+  public altura: number | undefined;
+  public valor_IMC: number | undefined;
+  public situacao: string = "";
+  constructor(private alertController: AlertController) { }
+
+  async calcularIMC() {
+    if (this.peso == null || this.altura == null) {
+      const alert = await this.alertController.create({
+        header: 'Atenção',
+        message: 'Por favor, preencha os dois campos antes de calcular o IMC',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    this.valor_IMC = this.peso / (this.altura * this.altura);
+
+    if (this.valor_IMC < 18.5) {
+      this.situacao = 'Abaixo do peso';
+    } else if (this.valor_IMC >= 18.5 && this.valor_IMC <= 24.9) {
+      this.situacao = 'Peso normal';
+    } else if (this.valor_IMC >= 25.0 && this.valor_IMC <= 29.9) {
+      this.situacao = 'Sobrepeso';
+    } else {
+      this.situacao = 'obesidade';
+    }
+  }
 }
